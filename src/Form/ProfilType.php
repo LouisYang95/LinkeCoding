0ce643a1f608ddd11206ec96e3212b5afd83f165
FlorianCohenJoly <floriancohenjoy@gmail.com>
2022-11-03 14:45:05 +0100

    Ajout du formulaire de création de profil

---
+++ src/Form/ProfilType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Profil;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ProfilType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nameProfil')
+            ->add('first_name')
+        
+            ->add('career')
+            ->add('skillsProfil')
+            ->add("submit", SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Profil::class,
+        ]);
+    }
+}
