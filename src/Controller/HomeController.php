d0ef534cd04edf4584d290da9667bce3cb13773b
Adrien140401 <91193459+Adrien140401@users.noreply.github.com>
2022-11-03 16:51:59 +0100

    add HomeController
    create homecontroller homepage

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Profil;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * Class ProfilController
+ * @package Home\Controller
+ * @Route("/homepage", name="homepage_"),
+ */
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $profils = $doctrine->getRepository(Profil::class)->findAll();
+
+
+
+        return $this->render('base.html.twig',[
+            'profils' => $profils,
+
+        ]);
+
+    }
+}
